87011f3eafb5b844fd72557b003c983b53acea9f
valkonetbanu343 <valkonetbanu343@gmail.com>
2025-04-07 14:30:18 +0000

    Add 28.py

---
+++ src/28.py
@@ -0,0 +1,6 @@
+import math
+pi = 3.14159
+radius = float(input("Enter the radius of the circle: "))
+area = pi * (radius ** 2)
+circumference = 2 * pi * radius
+print(f"Area: {area:.2f}, Circumference: {circumference:.2f}")
