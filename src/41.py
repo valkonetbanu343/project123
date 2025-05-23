f600ff2c0e9320ea99165a4182b3893f9d4a38b5
valkonetbanu343 <valkonetbanu343@gmail.com>
2025-05-23 07:23:28 +0000

    Add 41.py

---
+++ src/41.py
@@ -0,0 +1,15 @@
+def add_numbers(x: int, y: int) -> int:
+    """
+    Add two numbers.
+    
+    Args:
+        x (int): First number.
+        y (int): Second number.
+        
+    Returns:
+        int: The sum of x and y.
+    """
+    return x + y
+
+if __name__ == "__main__":
+    print(add_numbers(5, 10))
